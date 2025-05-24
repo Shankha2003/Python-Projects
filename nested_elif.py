8322a8530b46b6dd31652760cafb63b7d6c48576
Shankha2003 <pariashankhadeep@gmail.com>
2025-05-24 23:16:33 +0530

    Nested elif

---
+++ nested_elif.py
@@ -0,0 +1,14 @@
+weight = 85
+height = 1.85
+
+bmi = weight / (height ** 2)
+
+if bmi>=25:
+    print("overweight")
+
+elif bmi<18.5:
+        print("underweight")
+        
+else :
+    print("normal weight")
+
