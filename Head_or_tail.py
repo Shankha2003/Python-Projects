40cc451bc6fcbce5977fd2bc6da4febfd5ca94de
Shankha2003 <pariashankhadeep@gmail.com>
2025-06-02 18:11:34 +0530

    It's random Head or tail generetor

---
+++ Head_or_tail.py
@@ -0,0 +1,17 @@
+import random
+
+#random_integer = random.randint(1,10)
+#print(random_integer)
+
+#random_integer_0_to_10 = random.random() * 100
+#print(random_integer_0_to_10)
+
+#random_float = random.uniform(0, 10)
+#print(random_float)
+
+random_integer = random.randint(1,2)
+if random_integer == 1:
+    print(" It's a head")
+else:
+    print(" It's a tail")
+
